7c1110566766f97421d58bfb984a5d1ebb0f9a79
ifanramadan <cipanramadan5@gmail.com>
2024-11-12 17:03:35 +0700

    Add files via upload

---
+++ data/ADMINISTRASIDESA_AR_25K_10.js
@@ -0,0 +1,1 @@
+var json_ADMINISTRASIDESA_AR_25K_10 = {"type":"FeatureCollection","name":"ADMINISTRASIDESA_AR_25K_10","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
