4d571b37476fc4e142ce20d566548ba4bbaba9b9
ifanramadan <cipanramadan5@gmail.com>
2024-11-13 11:15:37 +0700

    Update Point_14.js

--- data/Point_14.js
+++ data/Point_14.js
@@ -1,1 +1,359 @@
-var json_Point_14 = {"type":"FeatureCollection","name":"Point_14","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Nama":"Masjid Al-Ikhlas"},"geometry":{"type":"Point","coordinates":[109.26158844004064,-7.409041542533837]}},{"type":"Feature","properties":{"Nama":"Masjid Baitul Fikri"},"geometry":{"type":"Point","coordinates":[109.261758380738584,-7.410061186721509]}},{"type":"Feature","properties":{"Nama":"Masjid Nurul Hidayah"},"geometry":{"type":"Point","coordinates":[109.262961242241161,-7.412570467339606]}},{"type":"Feature","properties":{"Nama":"Masjid Al Irsyad"},"geometry":{"type":"Point","coordinates":[109.265173126637862,-7.417700552158831]}},{"type":"Feature","properties":{"Nama":"Masjid Jami Syahlahudin Al Ayubi"},"geometry":{"type":"Point","coordinates":[109.264312801854516,-7.417180108771372]}},{"type":"Feature","properties":{"Nama":"Masjid dan TPQ Nurul Iman"},"geometry":{"type":"Point","coordinates":[109.261933632083256,-7.419389337844663]}},{"type":"Feature","properties":{"Nama":"Masjid Darul Falah Perumahan"},"geometry":{"type":"Point","coordinates":[109.258120587673133,-7.415108956515166]}},{"type":"Feature","properties":{"Nama":"Masjid Asy Syakuur"},"geometry":{"type":"Point","coordinates":[109.258449847775381,-7.414137108148791]}},{"type":"Feature","properties":{"Nama":"Masjid Al Huda"},"geometry":{"type":"Point","coordinates":[109.260574106499703,-7.411354329219936]}},{"type":"Feature","properties":{"Nama":"MAN 1 Banyumas"},"geometry":{"type":"Point","coordinates":[109.261235265593328,-7.419680334055996]}},{"type":"Feature","properties":{"Nama":"SD Negeri 1 Arcawinangun"},"geometry":{"type":"Point","coordinates":[109.261170655071567,-7.418137757848816]}},{"type":"Feature","properties":{"Nama":"TK Diponegoro 125"},"geometry":{"type":"Point","coordinates":[109.262898986528825,-7.417798552609542]}},{"type":"Feature","properties":{"Nama":"SD Negeri 4 Arcawinangun"},"geometry":{"type":"Point","coordinates":[109.263044360202798,-7.418081223642271]}},{"type":"Feature","properties":{"Nama":"SD Negeri 3 Arcawinangun"},"geometry":{"type":"Point","coordinates":[109.262164041843718,-7.415609871184697]}},{"type":"Feature","properties":{"Nama":"SD Negeri 1 Klahang"},"geometry":{"type":"Point","coordinates":[109.260403405125587,-7.415391810673734]}},{"type":"Feature","properties":{"Nama":"SD Negeri 5 Arcawinangun"},"geometry":{"type":"Point","coordinates":[109.261307952430315,-7.410206816301961]}},{"type":"Feature","properties":{"Nama":"GOR Satria Purwokerto"},"geometry":{"type":"Point","coordinates":[109.25086527684978,-7.416054068521843]}},{"type":"Feature","properties":{"Nama":"SMP Al Irsyad"},"geometry":{"type":"Point","coordinates":[109.251236787349939,-7.417733942087776]}},{"type":"Feature","properties":{"Nama":"SMA IT Al Irsyad"},"geometry":{"type":"Point","coordinates":[109.252020189926355,-7.417701636826893]}},{"type":"Feature","properties":{"Nama":"Bumi Arca Indah"},"geometry":{"type":"Point","coordinates":[109.258491337497105,-7.412677663989836]}},{"type":"Feature","properties":{"Nama":"ADA Barbershop"},"geometry":{"type":"Point","coordinates":[109.259815853193317,-7.413654898131556]}},{"type":"Feature","properties":{"Nama":"Puskesmas Pembantu Arcawinangun"},"geometry":{"type":"Point","coordinates":[109.260720400498059,-7.412007329826507]}},{"type":"Feature","properties":{"Nama":"SPBU Arcawinangun"},"geometry":{"type":"Point","coordinates":[109.261293818878741,-7.418678366198914]}},{"type":"Feature","properties":{"Nama":"Klinik Pratama Amanda"},"geometry":{"type":"Point","coordinates":[109.257950224377311,-7.421068955504279]}},{"type":"Feature","properties":{"Nama":"Warung Ora Umum"},"geometry":{"type":"Point","coordinates":[109.254445103571484,-7.41699849263298]}},{"type":"Feature","properties":{"Nama":"Ramen Tiger Diner"},"geometry":{"type":"Point","coordinates":[109.262408350379204,-7.412738236353992]}},{"type":"Feature","properties":{"Nama":"Rocket Chicken "},"geometry":{"type":"Point","coordinates":[109.262949463498998,-7.417402308394023]}},{"type":"Feature","properties":{"Nama":"Noris Autocar"},"geometry":{"type":"Point","coordinates":[109.263430004254644,-7.418593564889096]}},{"type":"Feature","properties":{"Nama":"Iqbal Rental Playstation"},"geometry":{"type":"Point","coordinates":[109.264488001548571,-7.417523453122335]}},{"type":"Feature","properties":{"Nama":"Rujak Mbah Nur"},"geometry":{"type":"Point","coordinates":[109.262501228004268,-7.417168095252619]}},{"type":"Feature","properties":{"Nama":"Cherry Petshop"},"geometry":{"type":"Point","coordinates":[109.263135218749099,-7.416828890013345]}},{"type":"Feature","properties":{"Nama":"Dokter Jok Purwokerto"},"geometry":{"type":"Point","coordinates":[109.254360302261702,-7.415920304551003]}},{"type":"Feature","properties":{"Nama":"Nasi goreng Suramadu"},"geometry":{"type":"Point","coordinates":[109.253051939195927,-7.417382117605973]}},{"type":"Feature","properties":{"Nama":"Kedai Baladewa"},"geometry":{"type":"Point","coordinates":[109.258681130904833,-7.413650859973949]}},{"type":"Feature","properties":{"Nama":"Warmindo"},"geometry":{"type":"Point","coordinates":[109.260256012372892,-7.413343959995557]}},{"type":"Feature","properties":{"Nama":"B11N GYM"},"geometry":{"type":"Point","coordinates":[109.261968191199699,-7.409794419456002]}},{"type":"Feature","properties":{"Nama":"Kost Erma"},"geometry":{"type":"Point","coordinates":[109.259537220318236,-7.411078553576114]}},{"type":"Feature","properties":{"Nama":"Relax Homestay"},"geometry":{"type":"Point","coordinates":[109.261075758367781,-7.410703004918346]}},{"type":"Feature","properties":{"Nama":"Gelora Arca Mas"},"geometry":{"type":"Point","coordinates":[109.259904692660783,-7.413416646832546]}},{"type":"Feature","properties":{"Nama":"Andika Aquarium"},"geometry":{"type":"Point","coordinates":[109.260514454459965,-7.413182433691142]}},{"type":"Feature","properties":{"Nama":"Makam Astana Duwur"},"geometry":{"type":"Point","coordinates":[109.263458271357962,-7.412366725853837]}},{"type":"Feature","properties":{"Nama":"Amira Florist Purwokerto"},"geometry":{"type":"Point","coordinates":[109.264807015999835,-7.418650099095643]}},{"type":"Feature","properties":{"Nama":"Kelompok Tani Ikan Mina Mulya"},"geometry":{"type":"Point","coordinates":[109.265008923880359,-7.418318970171589]}},{"type":"Feature","properties":{"Nama":"Bengkel Las"},"geometry":{"type":"Point","coordinates":[109.261649176748492,-7.417769780736572]}},{"type":"Feature","properties":{"Nama":"Dokter Laptop"},"geometry":{"type":"Point","coordinates":[109.262287205650935,-7.417624407062598]}}]}
+var json_Point_14 = {
+  type: "FeatureCollection",
+  name: "Point_14",
+  crs: { type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  features: [
+    {
+      type: "Feature",
+      properties: { Nama: "Masjid Al-Ikhlas" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.26158844004064, -7.409041542533837],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Masjid Baitul Fikri" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.261758380738584, -7.410061186721509],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Masjid Nurul Hidayah" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.262961242241161, -7.412570467339606],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Masjid Al Irsyad" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.265173126637862, -7.417700552158831],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Masjid Jami Syahlahudin Al Ayubi" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.264312801854516, -7.417180108771372],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Masjid dan TPQ Nurul Iman" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.261933632083256, -7.419389337844663],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Masjid Darul Falah Perumahan" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.258120587673133, -7.415108956515166],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Masjid Asy Syakuur" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.258449847775381, -7.414137108148791],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Masjid Al Huda" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.260574106499703, -7.411354329219936],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "MAN 1 Banyumas" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.261235265593328, -7.419680334055996],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "SD Negeri 1 Arcawinangun" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.261170655071567, -7.418137757848816],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "TK Diponegoro 125" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.262898986528825, -7.417798552609542],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "SD Negeri 4 Arcawinangun" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.263044360202798, -7.418081223642271],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "SD Negeri 3 Arcawinangun" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.262164041843718, -7.415609871184697],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "SD Negeri 5 Arcawinangun" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.261307952430315, -7.410206816301961],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "GOR Satria Purwokerto" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.25086527684978, -7.416054068521843],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "SMP Al Irsyad" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.251236787349939, -7.417733942087776],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "SMA IT Al Irsyad" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.252020189926355, -7.417701636826893],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Bumi Arca Indah" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.258491337497105, -7.412677663989836],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "ADA Barbershop" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.259815853193317, -7.413654898131556],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Puskesmas Pembantu Arcawinangun" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.260720400498059, -7.412007329826507],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "SPBU Arcawinangun" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.261293818878741, -7.418678366198914],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Klinik Pratama Amanda" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.257950224377311, -7.421068955504279],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Warung Ora Umum" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.254445103571484, -7.41699849263298],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Ramen Tiger Diner" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.262408350379204, -7.412738236353992],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Rocket Chicken " },
+      geometry: {
+        type: "Point",
+        coordinates: [109.262949463498998, -7.417402308394023],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Noris Autocar" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.263430004254644, -7.418593564889096],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Iqbal Rental Playstation" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.264488001548571, -7.417523453122335],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Rujak Mbah Nur" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.262501228004268, -7.417168095252619],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Cherry Petshop" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.263135218749099, -7.416828890013345],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Dokter Jok Purwokerto" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.254360302261702, -7.415920304551003],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Nasi goreng Suramadu" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.253051939195927, -7.417382117605973],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Kedai Baladewa" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.258681130904833, -7.413650859973949],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Warmindo" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.260256012372892, -7.413343959995557],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "B11N GYM" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.261968191199699, -7.409794419456002],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Kost Erma" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.259537220318236, -7.411078553576114],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Relax Homestay" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.261075758367781, -7.410703004918346],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Gelora Arca Mas" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.259904692660783, -7.413416646832546],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Andika Aquarium" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.260514454459965, -7.413182433691142],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Makam Astana Duwur" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.263458271357962, -7.412366725853837],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Amira Florist Purwokerto" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.264807015999835, -7.418650099095643],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Kelompok Tani Ikan Mina Mulya" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.265008923880359, -7.418318970171589],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Bengkel Las" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.261649176748492, -7.417769780736572],
+      },
+    },
+    {
+      type: "Feature",
+      properties: { Nama: "Dokter Laptop" },
+      geometry: {
+        type: "Point",
+        coordinates: [109.262287205650935, -7.417624407062598],
+      },
+    },
+  ],
+};
